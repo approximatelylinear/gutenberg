a5141dfb81f96ed4f36cb38a0e04339ecfe88a9c
MJ Berends <mjr.berends@gmail.com>
2024-04-14 23:08:55 -0500

    Add search endpoint

--- api/gutenberg/api.py
+++ api/gutenberg/api.py
@@ -1,6 +1,11 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+
+from gutenberg.client import make_client
+from gutenberg.es_index import EsIndex
+from gutenberg.search import Searcher
+
 
 app = FastAPI()
 
@@ -15,3 +20,28 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
+
+@app.get("/search")
+def search(
+        index: str,
+        query: str,
+        page_number: int = Query(1, ge=1),
+        highlight: bool = False
+    ):
+    es_client = make_client()
+    searcher = Searcher(es_index=EsIndex(es_client, index))
+    hits = searcher.search(
+        query, highlight=highlight, page_number=page_number
+    )
+    total_results = len(hits)
+    total_pages = 1  # Assuming 1 page for now
+    current_page_results = hits
+
+    response = {
+        "current_page": current_page_results,
+        "highlighted": highlight,
+        "total_results": total_results,
+        "total_pages": total_pages
+    }
+
+    return response
