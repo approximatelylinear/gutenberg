a2b69ff90410d647c7e879fbbdcb5e184e5ca732
MJ Berends <mjr.berends@gmail.com>
2024-04-14 23:09:17 -0500

    Add some logging statements to client.py

--- api/gutenberg/client.py
+++ api/gutenberg/client.py
@@ -8,6 +8,8 @@ from gutenberg.settings import CERT_PATH, ELASTIC_PASSWORD, ELASTIC_USER, ELASTI
 
 @cache
 def make_client():
+    print(f'Creating Elasticsearch client with url: {ELASTIC_URL}')
+    print(f'Using cert path: {CERT_PATH}')
     return Elasticsearch(
         ELASTIC_URL,
         basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
