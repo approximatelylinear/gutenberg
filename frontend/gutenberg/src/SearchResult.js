6cd4bf0c3b57aac6a79a46657201c8acf5d62838
MJ Berends <mjr.berends@gmail.com>
2024-04-15 02:39:33 -0500

    remove cruft

--- frontend/gutenberg/src/SearchResult.js
+++ frontend/gutenberg/src/SearchResult.js
@@ -11,7 +11,6 @@ const SearchResult = ({ title, content, author }) => {
         <div className="search-result">
             <h3 onClick={handleContentClick}>{title}</h3>
             <p><em>Author: {author}</em></p>
-            {/* <p>{content}</p> */}
             <p><em>Content: </em>{expanded ? `${content.substr(0, 1000)}` : `${content.substr(0, 100)}${content.length > 100 ? '...' : ''}`}</p>
 
         </div>
